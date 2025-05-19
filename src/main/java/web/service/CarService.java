a9eb0f4ccb3b4b42759f9955c7c598378d1d06d3
ФÐAlexandr_Stark0v <alexstarzakamsk@gmail.com>
2025-05-19 21:53:06 +0300

    Добавил на гит хаб

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,4 +1,8 @@
 package web.service;
 
+import web.model.Car;
+import java.util.List;
+
 public interface CarService {
+    public List<Car> getCars(Integer count);
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,4 +1,31 @@
 package web.service;
 
-public class CarServiceImpl {
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService{
+    private final List<Car> cars;
+
+    public CarServiceImpl() {
+        cars = new ArrayList<Car>();
+        cars.add(new Car("Volga", "110",1987));
+        cars.add(new Car("Gigul", "01",1991));
+        cars.add(new Car("BMW", "X5",2020));
+        cars.add(new Car("AUDI", "RS7",2023));
+        cars.add(new Car("Tesla", "S",2018));
+    }
+
+
+
+    @Override
+    public List<Car> getCars(Integer count) {
+        if(count == null || count >= cars.size()) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
 }
